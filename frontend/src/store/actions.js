2b9aa73e7de513693a8b4798142493d50d1d8bdb
NahyunLeylaLee <53142273+NahyunLeylaLee@users.noreply.github.com>
2022-07-20 14:08:57 +0900

    [S07P12A302-31] test: Add sample code
    sample url address added

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const actions = {
     getData(context) {
-        axios.get('url 들어가는 곳').then((a) => {
+        axios.get('https://jsonplaceholder.typicode.com/users/').then((a) => {//test url 입력되있습니다
             console.log(a.data);
             context.commit('setData', a.data);
         })
