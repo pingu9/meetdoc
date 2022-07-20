15d518d87dc6a0f40d685ff5898904de4979186b
NahyunLeylaLee <53142273+NahyunLeylaLee@users.noreply.github.com>
2022-07-20 17:19:48 +0900

    [S07P12A302-43] feat : Add Doctor List page
    Add Doctor List page

--- frontend/src/store/state.js
+++ frontend/src/store/state.js
@@ -1,4 +1,9 @@
 export const state = {
     name: 'leyla',//state(데이터) 보관하고 싶으면 여기에 하셈
     value: {},
+    doctors :[
+        {name: '의사1', hospital:'병원1', department:'진료과1' },
+        {name: '의사2', hospital:'병원2', department:'진료과2' },
+        {name: '의사3', hospital:'병원3', department:'진료과3' },
+    ],
 };
--- frontend/src/views/DoctorList.vue
+++ frontend/src/views/DoctorList.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    <h1>doctor list</h1>
+    <h1>doctor list - 진료과</h1>
+    <div class="card w-75" v-for="(list, idx) in $store.state.doctors" :key="idx">
+        <div class="card-body">
+        <h5 class="card-title">{{list.name}}</h5>
+        <p class="card-text">{{list.department}}</p>
+        <a href="#" class="btn btn-primary">예약하기</a>
+  </div>
+</div>
   </div>
 </template>
 
 <script>
+import {mapState } from 'vuex';
 export default {
-
+data(){
+    return{
+        
+    }
+},
+computed: {
+    ...mapState(['doctors']),
+  },
 }
 </script>
 
