87a4aa13da14538ca35ecb209bb3cdbeac3974f1
이나현 <florence5107@gmail.com>
2022-07-21 11:37:00 +0900

    [S07P12A302-62] feat : Add Hompage specialty list and alert
    Add Hompage specialty list and alert

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
     <h4>HomePage 입니다.</h4>
-    <router-link to="/doctors/1">진료과</router-link>
+    <div class="alert alert-primary" role="alert">
+    예약하신 진료시간이 다 되었습니다. <a href="#" class="alert-link">진료실 링크여기</a> 진료실에 입장해주세요!
+    </div>
+    <router-link to="/doctors/1">
+    <div class="row row-cols-1 row-cols-md-3 g-4" id="container-specialty">
+      <div class="col" v-for="(specialty, idx) in $store.state.specialties" :key="idx">
+        <div class="card h-100">
+          <img src="../assets/images/hospital.png" class="card-img-top" alt="specialty">
+          <div class="card-body">
+            <h5 class="card-title">{{specialty}}</h5>
+            <p class="card-text">진료과별 detail</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </router-link>
     <!-- <p>{{$store.state.name}}</p>
   <button @click="changeName()">한/영변환</button>
   <hr>
@@ -24,7 +39,7 @@ export default {
     
   },
   computed: {
-    ...mapState(['name', 'value']),
+    ...mapState(['name', 'value', 'specialties']),
     // ...mapGetters({
     //   getter : 'getName'
     // }),
@@ -38,5 +53,21 @@ export default {
 </script>
 
 <style>
+a{
+  color: black;
+  text-decoration: none;
+}
+
+a:hover{
+  cursor: pointer;
+}
+
+#container-specialty{
+  margin: 5%;
+}
+
+.alert-link{
+  text-decoration: underline;
+}
 
 </style>
