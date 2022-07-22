7eda69c071bae4515f035879ae8509839793d4f5
pingu9 <djg4053@gmail.com>
2022-07-22 11:06:59 +0900

    [S07P12A032-56] test: update dummy SQL file
    진료과  데이터 개수가  많아서 일부 제외하고 제거하였습니다.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,30 +1,13 @@
 insert into medic_department (department_name) values ('내과');
-insert into medic_department (department_name) values ('신경과');
-insert into medic_department (department_name) values ('정신과');
 insert into medic_department (department_name) values ('외과');
-insert into medic_department (department_name) values ('정형외과');
-insert into medic_department (department_name) values ('신경외과');
-insert into medic_department (department_name) values ('흉부외과');
-insert into medic_department (department_name) values ('성형외과');
+insert into medic_department (department_name) values ('정신과');
 insert into medic_department (department_name) values ('산부인과');
+insert into medic_department (department_name) values ('피부과');
 insert into medic_department (department_name) values ('안과');
 insert into medic_department (department_name) values ('이비인후과');
-insert into medic_department (department_name) values ('피부과');
-insert into medic_department (department_name) values ('비뇨기과');
-insert into medic_department (department_name) values ('진단검사의학과');
-insert into medic_department (department_name) values ('결핵과');
-insert into medic_department (department_name) values ('재활의학과');
-insert into medic_department (department_name) values ('가정의학과');
-insert into medic_department (department_name) values ('핵의학과');
 insert into medic_department (department_name) values ('소아청소년과');
-insert into medic_department (department_name) values ('마취통증의학과');
-insert into medic_department (department_name) values ('방사선종양학과');
-insert into medic_department (department_name) values ('병리과');
-insert into medic_department (department_name) values ('예방의학과');
-insert into medic_department (department_name) values ('응급의학과');
-insert into medic_department (department_name) values ('산업의학과');
-insert into medic_department (department_name) values ('치과');
-insert into medic_department (department_name) values ('기타진료과');
+insert into medic_department (department_name) values ('비뇨기과');
+
 
 
 insert into `user` (`user_type`, `name`, `gender`, `birthdate`, `status`) values ('D', 'doctor1', 'male', '2000-01-01', 'active');
@@ -52,6 +35,32 @@ insert into user_info (`user_id`, `password`, `RRN`, `zipcode`, `address`, `phon
 insert into doctor (`user_id`, `license_number`, `hospital_name`, `hospital_phone`, `hospital_zipcode`, `hospital_address`, `hospital_description`, `charge`) values (1, '1111', 'hospital1', '010-0001-0001', '123-456', 'address', 'description', 10000.0);
 insert into doctor (`user_id`, `license_number`, `hospital_name`, `hospital_phone`, `hospital_zipcode`, `hospital_address`, `hospital_description`, `charge`) values (2, '2222', 'hospital2', '010-0001-0001', '123-456', 'address', 'description', 10000.0);
 insert into doctor (`user_id`, `license_number`, `hospital_name`, `hospital_phone`, `hospital_zipcode`, `hospital_address`, `hospital_description`, `charge`) values (3, '3333', 'hospital3', '010-0001-0001', '123-456', 'address', 'description', 10000.0);
+
+
+
+
+
+
+-- insert into medic_department (department_name) values ('신경과');
+-- insert into medic_department (department_name) values ('정형외과');
+-- insert into medic_department (department_name) values ('신경외과');
+-- insert into medic_department (department_name) values ('흉부외과');
+-- insert into medic_department (department_name) values ('성형외과');
+-- insert into medic_department (department_name) values ('진단검사의학과');
+-- insert into medic_department (department_name) values ('결핵과');
+-- insert into medic_department (department_name) values ('재활의학과');
+-- insert into medic_department (department_name) values ('가정의학과');
+-- insert into medic_department (department_name) values ('핵의학과');
+-- insert into medic_department (department_name) values ('마취통증의학과');
+-- insert into medic_department (department_name) values ('방사선종양학과');
+-- insert into medic_department (department_name) values ('병리과');
+-- insert into medic_department (department_name) values ('예방의학과');
+-- insert into medic_department (department_name) values ('응급의학과');
+-- insert into medic_department (department_name) values ('산업의학과');
+-- insert into medic_department (department_name) values ('치과');
+-- insert into medic_department (department_name) values ('기타진료과');
+
+
 
 -- insert into medic_department (department_name) values ('한방과')
 -- insert into medic_department (department_name) values ('한방내과')
