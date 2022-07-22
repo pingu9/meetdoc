8ac533cfbdd8ea779b266dc3d872ae2a1bf1a57c
황준원 <miso97o@naver.com>
2022-07-22 13:55:17 +0900

    S07P12A302-70/feat:get department list

---
+++ backend/src/main/java/com/meetdoc/db/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.meetdoc.db.repository;
+
+import com.meetdoc.db.entity.MedicDepartment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<MedicDepartment, String> {
+
+}
