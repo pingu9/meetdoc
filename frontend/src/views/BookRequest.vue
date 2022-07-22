db55b6b09ab0d986c0eae6d50484ae6f46d49bc0
이나현 <florence5107@gmail.com>
2022-07-22 11:52:01 +0900

    [S07P12A302-101] fix : Fix router
    fix : Fix router

---
+++ frontend/src/views/BookRequest.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>진료 신청서 화면입니다.</h1>
+    <a href="/book/payment" class="btn btn-primary">다음</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
