9f7f998291407daafd647041289ec754056ec5dc
WOOSIEUN <ramtk6726@naver.com>
2022-07-22 16:43:55 +0900

    [S07P12A302-72] fix : Add unstaged files

--- backend/src/main/java/com/meetdoc/api/service/UserService.java
+++ backend/src/main/java/com/meetdoc/api/service/UserService.java
@@ -1,2 +1,8 @@
-package com.meetdoc.api.service;public interface UserService {
+package com.meetdoc.api.service;
+
+import com.meetdoc.db.entity.User;
+
+public interface UserService {
+    User getUserByUserId(String userId);
 }
+
