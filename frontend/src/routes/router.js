2255ea12406dfb96bbb7cc3e35f1927b92579491
이나현 <florence5107@gmail.com>
2022-07-22 11:52:01 +0900

    [S07P12A302-101] fix : Fix router
    fix : Fix router

--- frontend/src/routes/router.js
+++ frontend/src/routes/router.js
@@ -5,6 +5,11 @@ import HomePage from '../views/HomePage.vue';
 import DoctorList from '../views/DoctorList.vue';
 import ChartList from '../views/ChartList.vue';
 import BookHistory from '../views/BookHistory.vue';
+import SignUp from '../views/SignUp.vue';
+import ChartDetail from '../views/ChartDetail.vue';
+import BookRequest from '../views/BookRequest.vue';
+import BookPayment from '../views/BookPayment.vue';
+import BookConfirm from '../views/BookConfirm.vue';
 
 
 const routes = [
@@ -21,12 +26,32 @@ const routes = [
         component: DoctorList,
     },
     {
+        path: "/book/request",
+        component: BookRequest,
+    },
+    {
+        path: "/book/payment",
+        component: BookPayment,
+    },
+    {
+        path: "/book/confirm",
+        component: BookConfirm,
+    },
+    {
         path: "/chart/list",
         component: ChartList,
     },
     {
+        path: "/chart/detail/1",
+        component: ChartDetail,
+    },
+    {
         path: "/book/list",
         component: BookHistory,
+    },
+    {
+        path: "/signup",
+        component: SignUp,
     },
 ];
 
---
+++ frontend/src/views/BookPayment.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>결제 화면입니다</h1>
+    <a href="/book/confirm" class="btn btn-primary">결제완료</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
