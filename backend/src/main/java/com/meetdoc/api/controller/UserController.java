f54d62afabaf13f20939eddcb6afb063163fa51e
WOOSIEUN <ramtk6726@naver.com>
2022-07-23 11:57:17 +0900

    [S07P12A302-73] feat: Implement API to get userInfo

--- backend/src/main/java/com/meetdoc/api/controller/UserController.java
+++ backend/src/main/java/com/meetdoc/api/controller/UserController.java
@@ -2,14 +2,18 @@ package com.meetdoc.api.controller;
 
 import com.meetdoc.api.request.DoctorPostReq;
 import com.meetdoc.api.request.UserPostReq;
+import com.meetdoc.api.response.UserInfoGetRes;
 import com.meetdoc.api.service.UserService;
-import com.meetdoc.api.service.UserServiceImpl;
+import com.meetdoc.common.auth.UserDetails;
 import com.meetdoc.common.model.response.BaseResponseBody;
 import com.meetdoc.db.entity.User;
+import com.meetdoc.db.entity.UserInfo;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.NoSuchElementException;
 
@@ -45,7 +49,7 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    @ApiOperation(value = "아이디 중복 체크", notes = "해당 아이디가 존재하는지 체크한다.")
+    @ApiOperation(value = "아이디 중복 체크", notes = "해당 아이디의 중복 여부를 체크한다.")
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공"),
             @ApiResponse(code = 409, message = "아이디 중복."),
@@ -91,6 +95,7 @@ public class UserController {
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공"),
             @ApiResponse(code = 401, message = "권한 에러"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
             @ApiResponse(code = 406, message = "입력 형식 에러"),
             @ApiResponse(code = 500, message = "서버 문제로 인한 에러"),
     })
@@ -121,7 +126,7 @@ public class UserController {
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공"),
             @ApiResponse(code = 401, message = "권한 에러"),
-            @ApiResponse(code = 406, message = "입력 형식 에러"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
             @ApiResponse(code = 500, message = "서버 문제로 인한 에러"),
     })
 
@@ -147,31 +152,29 @@ public class UserController {
 
 
     @GetMapping("/info/{userId}")
-    @ApiOperation(value = "회원 탈퇴", notes = "회원 탈퇴")
+    @ApiOperation(value = "회원 정보 조회", notes = "회원 정보를 조회한다.")
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공"),
             @ApiResponse(code = 401, message = "권한 에러"),
-            @ApiResponse(code = 406, message = "입력 형식 에러"),
+            @ApiResponse(code = 404, message = "존재하지 않는 아이디"),
             @ApiResponse(code = 500, message = "서버 문제로 인한 에러"),
     })
 
-    public ResponseEntity<? extends BaseResponseBody> getUserInfo(
-            @PathVariable String userId) {
-        int code = 0; //dummy
+    public ResponseEntity<? extends BaseResponseBody> getUserInfo(@ApiIgnore Authentication authentication, @PathVariable String reqUserId) {
+        UserDetails userDetails = (UserDetails)authentication.getDetails();
+        String getUserId = userDetails.getUsername();
 
-        if (code == 0) {
-            return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+        //요청한 회원에 대한 권한이 없는 경우
+        if(!getUserId.equals(reqUserId)) {
+            return ResponseEntity.status(401).body(BaseResponseBody.of(401, "접근 권한이 없는 회원입니다."));
         }
-        else if (code == 1) {
-            String errMsg = "올바르지 않은 입력입니다.";
-            return ResponseEntity.status(406).body(BaseResponseBody.of(406, errMsg));
-        }
-        else if (code == 2) {
-            String errMsg = "권한이 없습니다.";
-            return ResponseEntity.status(401).body(BaseResponseBody.of(401, errMsg));
-        }
-        else {
-            return ResponseEntity.status(500).body(BaseResponseBody.of(500, "서버 에러 발생"));
+        User user = userService.getUserByUserId(getUserId);
+        UserInfo userInfo = userService.getUserInfoByUserId(getUserId);
+        //회원 존재여부 확인
+        if (user == null) {
+            return ResponseEntity.status(404).body(BaseResponseBody.of(404, "존재하지 않는 회원입니다."));
+        } else {
+            return ResponseEntity.status(200).body(UserInfoGetRes.of(200, "Success", user, userInfo));
         }
     }
 
--- backend/src/main/java/com/meetdoc/api/service/UserService.java
+++ backend/src/main/java/com/meetdoc/api/service/UserService.java
@@ -1,8 +1,10 @@
 package com.meetdoc.api.service;
 
 import com.meetdoc.db.entity.User;
+import com.meetdoc.db.entity.UserInfo;
 
 public interface UserService {
     User getUserByUserId(String userId);
+    UserInfo getUserInfoByUserId(String userId);
 }
 
