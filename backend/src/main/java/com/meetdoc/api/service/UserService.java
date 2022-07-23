1fff25828a05ab8be4604ee9dd9d749aa6e1ae8e
황준원 <miso97o@naver.com>
2022-07-23 16:54:45 +0900

    S07P12A302-71/feat:register doctor

--- backend/src/main/java/com/meetdoc/api/service/UserService.java
+++ backend/src/main/java/com/meetdoc/api/service/UserService.java
@@ -1,8 +1,11 @@
 package com.meetdoc.api.service;
 
+import com.meetdoc.api.request.DoctorPostReq;
+import com.meetdoc.db.entity.Doctor;
 import com.meetdoc.db.entity.User;
 
 public interface UserService {
     User getUserByUserId(String userId);
+    Doctor createDoctor(DoctorPostReq doctorInfo);
 }
 
