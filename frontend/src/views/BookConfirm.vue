ebe8d4c26fe13cb3b7d29f51680f8dd177bd8d35
NahyunLeylaLee <53142273+NahyunLeylaLee@users.noreply.github.com>
2022-07-25 14:22:54 +0900

    [S07P12A302-89] feat: Add Book Confirm Page
    Add Book Confirm Page

--- frontend/src/views/BookConfirm.vue
+++ frontend/src/views/BookConfirm.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-    <h1>결제완료 확인페이지입니다</h1>
-    <a href="/" class="btn btn-primary">확인</a>
+  <div class="container-body">
+    <h1>결제완료</h1>
+    <div class="container-card">
+     <div class="card">
+        <div class="card-body">
+          <img src="../assets/images/check.png" alt="confirmed" class="confirmed"/>
+          <p class="card-title">환자 님의 예약이 완료되었습니다.</p>
+          <div class="info-box">
+            <div class="half-box">
+              <p class="title-box">의사명</p>
+              <p class="title-box">진료과목</p>
+              <p class="title-box">결제금액</p>
+            </div>
+            <div class="half-box">
+              <p class="content-box">스트레인지</p>
+              <p class="content-box">내과</p>
+              <p class="content-box">10,000원</p>
+            </div>
+          </div>
+          <hr>
+          <p class="card-title" style="margin:20px 0">2022년 5월 28일 15:00</p>
+          <div class="d-grid gap-2">
+            <button class="btn btn-primary" type="button" a href="/">닫기</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +36,22 @@ export default {
 </script>
 
 <style>
+.title-box{
+  font-size: 25px;
+  color: gray;
+  font-weight: bold;
+  margin: 50px 0px;
+}
+
+.content-box{
+  font-size: 25px;
+  margin: 50px 0px;
+}
+
+.confirmed{
+  width: 50px;
+  height: 50px;
+  margin: 20px 0px;
+}
 
 </style>
--- frontend/src/views/BookPayment.vue
+++ frontend/src/views/BookPayment.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>결제 화면입니다</h1>
+    <a href="/book/request" class="btn btn-secondary" style="margin: 10px">이전</a>
     <a href="/book/confirm" class="btn btn-primary">결제완료</a>
   </div>
 </template>
