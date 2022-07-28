f2768f532de7b78f5539c9287b1927e02c63250e
WOOSIEUN <ramtk6726@naver.com>
2022-07-29 02:15:35 +0900

    [S07P12A302-139] chore: Add files for deploy (only dockerizing)

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    https: true,
+    port: 8083,
+    open: true,
+    proxy: {
+      '/api/v1': {
+        target: 'https://localhost:8443/'
+      },
+    },
+    historyApiFallback: true,
+    hot: true
+  },
+  // css : {
+  //   // import 할 때 경로에 .module 포함하지 않아도 됨
+  //   requireModuleExtension: false
+  // },
+  transpileDependencies: [
+    'element-plus'
+  ],
+  lintOnSave: false,
+  outputDir: '../backend/src/main/resources/dist'
+}
