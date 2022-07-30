8443c5b17a78607e636aec501b4c16da5685ee60
이나현 <florence5107@gmail.com>
2022-07-31 00:15:42 +0900

    [S07P12A302-96] feat: Add Book Request Time Select Desgin
    Add Book Request Time Select Desgin

--- frontend/src/views/ChartList.vue
+++ frontend/src/views/ChartList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-body">
-    <h1>Chart List</h1>
+    <h1>{{$store.state.chartList[0].doctorName}} 님의 진료내역</h1>
     <div class="card w-90" v-for="(chart, idx) in $store.state.chartList" :key="idx" id="container-card">
         <div class="card-body">
         <h5 class="card-title">{{chart.patientName}}</h5>
@@ -15,7 +15,8 @@
 import { mapState } from 'vuex';
 export default {
   data(){
-    return{
+    return {
+      doctorName: '',
     }
   },
   components: {
@@ -27,6 +28,7 @@ export default {
     //doctorId가 보내져야하는데 user8은 환자여서 user1 하드코딩
     const doctorId = localStorage.getItem('userId');
     this.$store.dispatch('getChartList', doctorId);
+
   },
   methods:{
     sendApptId(){
