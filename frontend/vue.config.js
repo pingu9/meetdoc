cc650965cf84c4f059e1a2c2b66c6d24227f1edb
pingu9 <djg4053@gmail.com>
2022-08-02 17:49:01 +0900

    [S07P12A302-176] change proxy property

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-const target = 'http://localhost:8081/';
+const { defineConfig } = require("@vue/cli-service");
+const target = "http://localhost:8081/";
 
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
 
 module.exports = {
   devServer: {
@@ -11,27 +11,19 @@ module.exports = {
     port: 8080,
     open: true,
     proxy: {
-      '/user': {
-        target: target
-      },
-      '/appointment': {
-        target: target
-      },
-      '/doctor': {
-        target: target
+      "/api": {
+        target: target,
       },
     },
     historyApiFallback: true,
-    hot: true
+    hot: true,
   },
   // css : {
   //   // import 할 때 경로에 .module 포함하지 않아도 됨
   //   requireModuleExtension: false
   // },
-  transpileDependencies: [
-    'element-plus'
-  ],
+  transpileDependencies: ["element-plus"],
   lintOnSave: false,
   // outputDir: '../backend/src/main/resources/dist'
-  outputDir: './dist'
-}
+  outputDir: "./dist",
+};
