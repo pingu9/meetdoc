ce4346619f23d73f627e6f155894429f454bfbbe
황준원 <miso97o@naver.com>
2022-08-03 15:32:12 +0900

    [S07P12A302-161]fix:encode password

--- backend/src/main/java/com/meetdoc/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/meetdoc/api/service/UserServiceImpl.java
@@ -75,6 +75,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Long updateUserByUserId(String userId, UserPatchReq patchUserReq) {
+        patchUserReq.setPassword((passwordEncoder.encode(patchUserReq.getPassword())));
         Long affectedRow = userRepositorySupport.updateUserByUserId(userId, patchUserReq);
         return affectedRow;
     }
