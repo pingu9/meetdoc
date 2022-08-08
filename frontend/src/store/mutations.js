cd1bf1ec0b9b08d0afd2760098715c7b036a199b
coolihans <97646033+coolihans@users.noreply.github.com>
2022-08-08 17:07:26 +0900

    [S07P12A302-2510] fix: navbar change

--- frontend/src/store/mutations.js
+++ frontend/src/store/mutations.js
@@ -43,5 +43,14 @@ export const mutations = {
     setLoginToken(state, loginToken) {
         state.loginToken = loginToken
         console.log(state.loginToken)
+<<<<<<< Updated upstream
     }
+=======
+    },
+    setphotoUrl(state, photoUrl){
+        state.photoUrl = photoUrl;
+    },
+    setToken: (state, token) => state.loginToken = token
+
+>>>>>>> Stashed changes
 };
