d3b259f413921166d587b1a43c41ff2023b68308
coolihans <97646033+coolihans@users.noreply.github.com>
2022-08-11 18:18:19 +0900

    S07P12A302-251/logindialog change
    로그인 모달 창 추가

--- frontend/src/components/LoginDialog.vue
+++ frontend/src/components/LoginDialog.vue
@@ -1,6 +1,6 @@
 <template>
 <form @submit.prevent="loginForm" class="modal" tabindex="-1" id="loginModal">
-  <div class="modal-dialog modal-dialog-centered">
+  <!-- <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title">MeetDoc Login</h5>
@@ -23,6 +23,35 @@
       <span>
         <button type="button" class="btn btn-secondary">비밀번호 찾기</button>
       </span>
+      </div>
+    </div>
+  </div> -->
+  <div class="modal-dialog">
+    <div class="modal-content" style="border-radius: 1rem;">
+      <div class="row d-flex justify-content-center align-items-center" >
+        <div class="col-12">
+          <div>
+            <div class="modal-header">
+              <h3>MeetDoc Login</h3>
+            </div>
+            <div class="modal-body text-center p-5">
+              <div class="form-floating mb-3">
+                <input @keyup.enter="loginForm()" type="username" v-model="username" class="form-control" id="floatingInput" placeholder="Username">
+                <label for="floatingInput">MeetDoc ID</label>
+              </div>
+              <div class="form-floating mb-5">
+                <input @keyup.enter="loginForm()" type="password" v-model="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+              </div>
+              <!-- <div class="form-check d-flex justify-content-start mb-4">
+                <input class="form-check-input " type="checkbox" value="" id="form1Example3" />
+                <label class="form-check-label" for="form1Example3"> Remember password </label>
+              </div> -->
+              <button class="btn btn-primary btn-block col-12" type="submit">Login</button>
+              <hr class="my-4">
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
