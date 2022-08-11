ef87468e7e4a3d25b848a5f0d64145482e6e12b7
황준원 <miso97o@naver.com>
2022-08-11 09:40:30 +0900

    [S07P12A302-207]:fix ApiResponse data

--- backend/src/main/java/com/meetdoc/api/response/NextAppointmentRes.java
+++ backend/src/main/java/com/meetdoc/api/response/NextAppointmentRes.java
@@ -12,16 +12,20 @@ import java.util.List;
 @Getter
 @Setter
 public class NextAppointmentRes extends BaseResponseBody {
+    int appointmentId;
     String status;
     String appointmentTime;
+    String patientName;
     String doctorName;
 
     public static NextAppointmentRes of(Integer statusCode, String message, Appointment ap) {
         NextAppointmentRes res = new NextAppointmentRes();
         res.setStatusCode(statusCode);
         res.setMessage(message);
+        res.setAppointmentId(ap.getAppointmentId());
         res.setStatus(ap.getStatus());
         res.setAppointmentTime(ap.getAppointmentTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+        res.setDoctorName(ap.getUser().getName());
         res.setDoctorName(ap.getDoctor().getUser().getName());
         return res;
     }
