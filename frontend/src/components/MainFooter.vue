5812f3163817476172266e4ee27171bf6643d7a4
WOOSIEUN <ramtk6726@naver.com>
2022-08-12 14:06:38 +0900

    [S07P12A302-261] style: Add footer

--- frontend/src/components/MainFooter.vue
+++ frontend/src/components/MainFooter.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="Mainfooter">
-    Copyright © SAMSUNG All Rights Reserved.
+    <br/><small class="text-bold">2022 MeetDoc</small><br/>
+    <small class="text-muted">(주) MeetDoc(믿닥) / 우시은, 권덕주, 안현모, 이나현, 황준원 / </small><br/>
+    <small class="text-muted">서울 강남구 테헤란로 212, 11층 1102 (2팀) (역삼동, 멀티캠퍼스 역삼)</small><br/>
+    <small>Copyright © 2022 MeetDoc All Rights Reserved.</small>
 </div>
 </template>
 
@@ -23,4 +26,7 @@ export default {
   transform : translateY(-100%);
 }
 
+.text-bold {
+  font-weight: bold;
+}
 </style>
