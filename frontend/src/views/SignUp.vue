31745e5f5291720cb6f4559af15a0fb7f03bdf9e
이나현 <florence5107@gmail.com>
2022-08-16 15:18:24 +0900

    [S07P12A302-293] feat: add zipcode api
    add zipcode api

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -21,10 +21,10 @@
         <input name="loginPwConfirm" type="password" class="rrn" maxlength="7" placeholder="주민번호 뒷자리" v-model="rrn2">
       </div>
       <div class="textForm">
-        <input name="nickname" type="text" class="nickname" placeholder="주소" v-model="address">
+        <input name="nickname" type="text" class="nickname" placeholder="주소" v-model="address"  @click="execDaumPostcode()">
       </div>
       <div class="textForm">
-        <input name="nickname" type="text" class="nickname" placeholder="우편번호" v-model="zipcode">
+        <input name="nickname" type="text" class="nickname" placeholder="우편번호" v-model="postcode" @click="execDaumPostcode()">
       </div>
       <div class="textForm">
         <input name="cellphoneNo" type="tel" class="cellphoneNo" placeholder="전화번호 예) 010-0000-0000" v-model="phone">
@@ -99,7 +99,7 @@ export default {
       rrn2: '',
       userName: '',
       phone:'',
-      zipcode: '',	    
+      postcode: '',	    
       address: '',
       email: '',
       birthdate: '',
@@ -132,6 +132,46 @@ export default {
   },
 
   methods: {
+    execDaumPostcode() {
+      new window.daum.Postcode({
+        oncomplete: (data) => {
+          if (this.extraAddress !== "") {
+            this.extraAddress = "";
+          }
+          if (data.userSelectedType === "R") {
+            // 사용자가 도로명 주소를 선택했을 경우
+            this.address = data.roadAddress;
+          } else {
+            // 사용자가 지번 주소를 선택했을 경우(J)
+            this.address = data.jibunAddress;
+          }
+ 
+          // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+          if (data.userSelectedType === "R") {
+            // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+            // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+            if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+              this.extraAddress += data.bname;
+            }
+            // 건물명이 있고, 공동주택일 경우 추가한다.
+            if (data.buildingName !== "" && data.apartment === "Y") {
+              this.extraAddress +=
+                this.extraAddress !== ""
+                  ? `, ${data.buildingName}`
+                  : data.buildingName;
+            }
+            // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+            if (this.extraAddress !== "") {
+              this.extraAddress = `(${this.extraAddress})`;
+            }
+          } else {
+            this.extraAddress = "";
+          }
+          // 우편번호를 입력한다.
+          this.postcode = data.zonecode;
+        },
+      }).open();
+    },
     checkId () {
       if (this.userId) {
         console.log(this.userId)
@@ -187,7 +227,7 @@ export default {
           rrn: this.rrn1 + this.rrn2,
           userName: this.userName,
           phone: this.phone,
-          zipcode: this.zipcode,
+          zipcode: this.postcode,
           address: this.address,
           email: this.email,
           birthdate: this.birthdate,
